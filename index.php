667ca96c929932a048f87cccd36b3c4707f78b87
sax1412 <1252735842@qq.com>
2016-05-13 18:42:26 +0800

    5.4客户反馈

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ define('APP_DEBUG',True);
 
 // 定义应用目录
 define('APP_PATH','./Application/');
-ini_set ( 'session.cookie_lifetime', 60 );
-ini_set ( 'session.gc_maxlifetime', 60 );
+ini_set ( 'session.cookie_lifetime', 3600 );
+ini_set ( 'session.gc_maxlifetime', 3600 );
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
