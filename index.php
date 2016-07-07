ac31d3099219644a6692ca0a2c30ba6144317134
sax1412 <1252735842@qq.com>
2016-07-07 10:57:14 +0800

    新需求

--- index.php
+++ index.php
@@ -12,15 +12,20 @@
 // 应用入口文件
 
 // 检测PHP环境
-if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
+if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',True);
+define('APP_DEBUG', True);
+if (strstr($_SERVER["REQUEST_URI"],'admin')) {
 
+} else {
+    header("Location:http://www.maisoncapital.com");
+    exit();
+};
 // 定义应用目录
-define('APP_PATH','./Application/');
-ini_set ( 'session.cookie_lifetime', 3600 );
-ini_set ( 'session.gc_maxlifetime', 3600 );
+define('APP_PATH', './Application/');
+ini_set('session.cookie_lifetime', 3600);
+ini_set('session.gc_maxlifetime', 3600);
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 
