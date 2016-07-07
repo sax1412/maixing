cec617cd48eb74b4be166c49f69beb4838ea1d2e
sax1412 <1252735842@qq.com>
2016-07-07 11:00:13 +0800

    新需求

--- index.php
+++ index.php
@@ -16,12 +16,12 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', True);
-if (strstr($_SERVER["REQUEST_URI"],'admin')) {
+/*if (strstr($_SERVER["REQUEST_URI"],'admin')) {
 
 } else {
     header("Location:http://www.maisoncapital.com");
     exit();
-};
+};*/
 // 定义应用目录
 define('APP_PATH', './Application/');
 ini_set('session.cookie_lifetime', 3600);
